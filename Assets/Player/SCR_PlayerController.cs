7795c4a39ff7b580fdade7b012fbde627eafb282
Justin Camden <justin@geistnet.com>
2021-01-26 15:50:52 -0800

    Refined gravity and jump controls

--- Assets/Player/SCR_PlayerController.cs
+++ Assets/Player/SCR_PlayerController.cs
@@ -7,7 +7,11 @@ using UnityEngine.InputSystem;
 
 public class SCR_PlayerController : MonoBehaviour
 {
-    // Bounding box
+    [Header("Physics")]
+    [SerializeField]
+    LayerMask collisionMask;
+
+    [Header("Bounding Box")]
     [SerializeField]
     BoxCollider2D boundingCollider;
     [SerializeField]
@@ -21,7 +25,7 @@ public class SCR_PlayerController : MonoBehaviour
     float halfWidth;
     float halfHeight;
 
-    // Raycast settings
+    [Header("Raycasting")]
     [SerializeField, Min(2)]
     int horizontalRayCount = 5;
     [SerializeField, Min(2)]
@@ -29,21 +33,31 @@ public class SCR_PlayerController : MonoBehaviour
     List<Vector2> horizontalRayOrigins = new List<Vector2>();
     List<Vector2> verticalRayOrigins = new List<Vector2>();
 
-    // Physics
-    [SerializeField]
-    float gravityAcceleration = 9.8f;
+    [Header("Gravity")]
     [SerializeField]
     float gravityMaxMagnitude = 20.0f;
     [SerializeField]
     float gravityDirection = -1.0f;
-    [SerializeField]
-    LayerMask collisionMask;
+    [SerializeField, Min(1.0f)]
+    float gravityFallingMultiplier = 3.0f;
+    float gravityAccelerationDefault;
+    float gravityAcceleration;
     bool isGrounded;
     bool isCollidingRight;
     bool isCollidingLeft;
     Vector2 velocity;
     Vector2 calculateMovement;
-    float jumpForce = 10.0f;
+
+    [Header("Controls")]
+    [Header("Jumping")]
+    [SerializeField]
+    float jumpTimeToApex = 0.6f;
+    [SerializeField, Min(0.1f)]
+    float jumpHeightMax = 3.0f;
+    [SerializeField, Min (0.1f)]
+    float jumpHeightMin = 1.0f;
+    float jumpVelocityMax;
+    float jumpVelocityMin;
 
     // Debug variables
 #if UNITY_EDITOR
@@ -54,14 +68,19 @@ public class SCR_PlayerController : MonoBehaviour
     // Use awake to initialize values
     void Awake()
     {
+        CalculateGravity();
+        CalculateJumpVelocities();
         CalculateBoundingBox();
         CalculateRayOrigins();
-        //playerInput.actions.FindAction<"Jump">
+
+        return;
     }
 
     private void FixedUpdate()
     {
         UpdateMovement();
+
+        return;
     }
 
     void UpdateMovement()
@@ -70,13 +89,30 @@ public class SCR_PlayerController : MonoBehaviour
         origin = boundingCollider.bounds.center;
 
         // Calculate new gravity
-        velocity.y = Mathf.Clamp(velocity.y + (gravityAcceleration * gravityDirection * Time.deltaTime), -gravityMaxMagnitude, gravityMaxMagnitude);
+        ApplyGravity();
 
         // Handle collisions
         UpdateVerticalCollisions();
 
         // Move the character
         transform.Translate(calculateMovement);
+
+        return;
+    }
+
+    void ApplyGravity()
+    {
+        // If not falling, apply normal gravity
+        if (velocity.y >= 0.0f)
+        {
+            velocity.y = Mathf.Clamp(velocity.y + (gravityAcceleration * gravityDirection * Time.deltaTime), -gravityMaxMagnitude, gravityMaxMagnitude);
+        }
+
+        // Otherwise, apply falling gravity
+        else
+        {
+            velocity.y = Mathf.Clamp(velocity.y + (gravityAcceleration * gravityDirection * Time.deltaTime * gravityFallingMultiplier), -gravityMaxMagnitude, gravityMaxMagnitude);
+        }
     }
 
     void UpdateVerticalCollisions()
@@ -138,6 +174,22 @@ public class SCR_PlayerController : MonoBehaviour
             isGrounded = false;
             calculateMovement.y = velocity.y * Time.deltaTime;
         }
+
+        return;
+    }
+
+    void CalculateGravity()
+    {
+        gravityAccelerationDefault = (2.0f * jumpHeightMax) / Mathf.Pow(jumpTimeToApex, 2.0f);
+        gravityAcceleration = gravityAccelerationDefault;
+        return;
+    }
+
+    void CalculateJumpVelocities()
+    {
+        jumpVelocityMin = Mathf.Sqrt(2.0f * gravityAccelerationDefault * jumpHeightMin);
+        jumpVelocityMax = Mathf.Max(jumpVelocityMin, gravityAccelerationDefault * jumpTimeToApex);
+        return;
     }
 
     void CalculateBoundingBox()
@@ -182,8 +234,13 @@ public class SCR_PlayerController : MonoBehaviour
     {
         if (context.performed && isGrounded)
         {
-            velocity += new Vector2(0.0f, jumpForce);
+            velocity.y += (jumpVelocityMax);
         }
+        else if (context.canceled && velocity.y > jumpVelocityMin)
+        {
+            velocity.y = jumpVelocityMin;
+        }
+        return;
     }
 
 #if UNITY_EDITOR
