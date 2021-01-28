574be710796519f212be9e38a4dada37600016a9
Eric Kim <ericpaulkim@gmail.com>
2021-01-27 22:53:46 -0800

    Add switch reseting

--- Assets/Interactables/Switches/SCR_Switch.cs
+++ Assets/Interactables/Switches/SCR_Switch.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class SCR_Switch : MonoBehaviour
 {
+    public ContactFilter2D m_Filter;
     public bool m_Retriggerable = false;
+    public float m_RetriggerDuration = 2.0f;
     public GameObject[] m_ObjectsToTrigger;
 
+    private float m_RetriggerTimer = 0.0f;
     private bool m_HasTriggered = false;
+    private Collider2D m_Collider;
+
+    private void Awake()
+    {
+        m_Collider = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (m_HasTriggered && !m_Retriggerable)
+        if (!CanTrigger())
         {
             return;
         }
 
-        SCR_ICanTrigger canTrigger = collision?.gameObject?.GetComponent<SCR_ICanTrigger>();
-        if(canTrigger == null)
+        SCR_ICanTrigger triggerObject = collision?.gameObject?.GetComponent<SCR_ICanTrigger>();
+        if(triggerObject == null)
         {
             return;
         }
+        Trigger();
+    }
+
+    private void Trigger()
+    {
         foreach (GameObject g in m_ObjectsToTrigger)
         {
             SCR_ITriggerable trigger = g.GetComponent<SCR_ITriggerable>();
@@ -32,13 +47,51 @@ public class SCR_Switch : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        if(m_RetriggerTimer > 0.0f)
+        {
+            m_RetriggerTimer -= Time.fixedDeltaTime;
+            if(m_RetriggerTimer <= 0.0f)
+            {
+                RevertTriggerFeedback();
+            }
+        }
+    }
+
+    private bool CanTrigger()
+    {
+        return !m_HasTriggered || (m_Retriggerable && m_RetriggerTimer == 0.0f);
+    }
+
+    private void RevertTriggerFeedback()
+    {
+        m_RetriggerTimer = 0.0f;
+        m_HasTriggered = false;
+        GetComponent<SpriteRenderer>().color = Color.red;
+        List<Collider2D> overlappingColliders = new List<Collider2D>();
+        int num = m_Collider.OverlapCollider(m_Filter, overlappingColliders);
+        if(num != 0)
+        {
+            foreach (Collider2D g in overlappingColliders)
+            {
+                SCR_ICanTrigger trigger = g.gameObject.GetComponent<SCR_ICanTrigger>();
+                if (trigger != null)
+                {
+                    Trigger();
+                }
+            }
+        }
+    }
+
     private void TriggerFeedback()
     {
-        if (m_HasTriggered && !m_Retriggerable)
+        if (!CanTrigger())
         {
             return;
         }
         m_HasTriggered = true;
+        m_RetriggerTimer = m_RetriggerDuration;
         GetComponent<SpriteRenderer>().color = Color.green;
     }
 }
