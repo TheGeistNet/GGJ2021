378210d400c4c8e4f10ec1048dcbf73664af53cf
Blair <blairdevx@gmail.com>
2021-01-28 16:40:36 -0800

    Credits and next scene trigger

---
+++ Assets/Interactables/SceneTrigger/SCR_NextSceneTrigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCR_NextSceneTrigger : MonoBehaviour
+{
+    [SerializeField] string nextSceneName = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<SCR_PlayerController>())
+        {
+            if (nextSceneName != "")
+            {
+                SceneManager.LoadSceneAsync(nextSceneName);
+            }
+        }
+    }
+}
