857bb5058b766dfdb3be3e9467b2534c9110e2fe
Justin Camden <justin@geistnet.com>
2021-01-30 19:57:59 -0800

    Fixed a bug

--- Assets/Blocks/SCR_BlockLayout.cs
+++ Assets/Blocks/SCR_BlockLayout.cs
@@ -17,25 +17,20 @@ public class SCR_BlockLayout : MonoBehaviour
     float endLength = 1.0f;
     [SerializeField, Min(0.1f)]
     float height = 0.5f;
+    [SerializeField]
     GameObject middlePiece;
+    [SerializeField]
     GameObject rightEndPiece;
+    [SerializeField]
     GameObject leftEndPiece;
 
 
+    [ExecuteInEditMode]
     public void LayoutBlock()
     {
-        // Clear old pieces
-        if (middlePiece)
+        foreach(Transform child in transform)
         {
-            DestroyImmediate(middlePiece);
-        }
-        if (leftEndPiece)
-        {
-            DestroyImmediate(leftEndPiece);
-        }
-        if (rightEndPiece)
-        {
-            DestroyImmediate(rightEndPiece);
+            DestroyImmediate(child.gameObject);
         }
 
 
@@ -47,7 +42,7 @@ public class SCR_BlockLayout : MonoBehaviour
 
             // Create middle piece
             middlePiece = Instantiate(middlePieceClass, transform);
-            middlePiece.transform.localScale = new Vector2(totalLength, height);
+            middlePiece.transform.localScale = new Vector3(totalLength, height, 1.0f);
 
             // Create ends
             // Left end
@@ -55,14 +50,14 @@ public class SCR_BlockLayout : MonoBehaviour
             {
                 leftEndPiece = Instantiate(leftEndClass, transform);
                 leftEndPiece.transform.localPosition = new Vector2(-halfLength - (endLength * 0.5f), 0.0f);
-                leftEndPiece.transform.localScale = new Vector2(endLength, height);
+                leftEndPiece.transform.localScale = new Vector3(endLength, height, 1.0f);
                 totalLength += endLength;
             }
             if (rightEndClass)
             {
                 rightEndPiece = Instantiate(leftEndClass, transform);
                 rightEndPiece.transform.localPosition = new Vector2(halfLength + (endLength * 0.5f), 0.0f);
-                rightEndPiece.transform.localScale = new Vector2(endLength, height);
+                rightEndPiece.transform.localScale = new Vector3(endLength, height, 1.0f);
                 totalLength += endLength;
             }
 
